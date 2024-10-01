24624f3208c105528c012463c981db7ed0b7d8ff
welles <welles.augusto@gmail.com>
2024-09-30 22:38:52 -0300

    Gerar personagem

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Coisas para fazer
 
 - [X] Perguntar o nome do personagem
-- [ ] Gerar o personagem
+- [X] Gerar o personagem
 - [ ] Apresentar o inimigo
 - [ ] Perguntar a ação do personagem
 - [ ] Efetuar a ação
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import random
+
+# Variaveis do jogo
+quantidade_espaco = 100
+
+# Variaveis do personagem
+vida_maxima = random.randint(100, 200)
+vida_atual = vida_maxima
+ataque_base = random.randint(30, 50)
+estamina_maxima = random.randint(1, 5)
+estamina_atual = estamina_maxima
+
+# -------------------------------------------------------------------------
+
+print('Seja bem vindo ao jogo RPG text')
 nome_personagem = input('Qual o nome do personagem: ')
+# nome_personagem = 'Kirito'
+
+print('=' * quantidade_espaco)
+print('{:^100}'.format(nome_personagem))
+print('=' * quantidade_espaco)
+print('Barra de vida ({}/{}): '.format(vida_atual, vida_maxima), end='')
+print('♡ ' * (vida_atual // 10))
+print('Ataque base: {}'.format(ataque_base))
+print('Estamina: {}'.format(estamina_atual))
+print('=' * quantidade_espaco)
